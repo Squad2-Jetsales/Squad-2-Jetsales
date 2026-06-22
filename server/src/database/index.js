8e28dd18c18859ed2511701e12f17233180c5727
George Gioseffi <georgegioseffi@hotmail.com>
2026-06-22 12:51:53 -0300

    corrige selecao do banco em producao

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -6,7 +6,19 @@
 const knex = require('knex');
 const config = require('../../knexfile');
 
-const env = process.env.NODE_ENV || 'development';
+function resolveEnv() {
+  if (process.env.NODE_ENV) return process.env.NODE_ENV;
+
+  const hasManagedDatabaseUrl =
+    process.env.DATABASE_URL ||
+    process.env.POSTGRES_URL ||
+    process.env.POSTGRES_PRISMA_URL ||
+    process.env.POSTGRES_URL_NON_POOLING;
+
+  return hasManagedDatabaseUrl ? 'production' : 'development';
+}
+
+const env = resolveEnv();
 const db = knex(config[env]);
 
 module.exports = db;
